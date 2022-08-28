c4ee4d98bb73e09052ef6b0b1be2744e9ccb47bc
Zdylas <dominic.mcnulty12@gmail.com>
2022-08-28 16:08:39 +1200

    Update/Testing

---
+++ ENCE260/testing.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+size_t readString(char* string, size_t maxStringLength)
+{
+	char chr;
+	for (i =0; i < maxStringLength; i++) {
+		scanf("%c", %d) 
+	}
+	}
+}
+
+
+int main(void)
+{
+	size_t len = 0;
+	char string[11];
+	len = readString(string, 10);
+	printf("Read String (%s) of length (%zu)\n", string, len);
+}
